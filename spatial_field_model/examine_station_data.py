d069435d3d1df4fca6031bab6c0d855d332142ec
Martin Vejmelka <vejmelkam@gmail.com>
2012-10-31 17:54:05 -0600

    - added many new visualizations and an investigation of parametrization of the residual covariance matrix for kriging - separated out WRF loading code into a new file wrf_model_data.py - now ready to write simple KF code for individual grid cells

--- spatial_field_model/examine_station_data.py
+++ spatial_field_model/examine_station_data.py
@@ -1,25 +1,26 @@
 
-from spatial_model_utilities import load_wrf_data, load_station_data, render_spatial_field, \
+from spatial_model_utilities import load_station_data, render_spatial_field, \
                                     equilibrium_moisture, load_stations_from_files, \
-                                    match_stations_to_gridpoints, match_sample_times
-import os
+                                    match_stations_to_gridpoints, match_sample_times, \
+                                    great_circle_distance
+                                    
+from wrf_model_data import WRFModelData
+
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
-from datetime import datetime
 from matplotlib.dates import DateFormatter
-import math
 
 
 station_list = [  "Julian_Moisture",
                   "Goose_Valley_Fuel_Moisture",
 #                  "Mt Laguna_Moisture",
                   "Oak_Grove_Moisture",
-                  "Decanso_Mositure",
+                  "Decanso_Moisture",
 #                  "Palomar_Fuel_Moisture",
                   "Alpine_Moisture",
 #                  "Valley_Center_Moisture",
-                  "Ranchita_Mositure",
+                  "Ranchita_Moisture",
                   "Camp_Elliot_Moisture",
 #                  "Pine Hills_Moisture" 
                 ]
@@ -27,109 +28,240 @@ station_list = [  "Julian_Moisture",
 station_data_dir = "../real_data/witch_creek/"
 
 
-def match_time_series(stations, st_field_name, field, mlon, mlat, mtimes):
+def match_time_series(stations, st_field_name, field, W):
     """
     Matches the time series of the field with the values of st_field in each station in stations.
-    Returns the matched time series indexed by station name.  The field passed in has to be valid
-    for the model longitude and latitude given.
+    Returns the matched time series indexed by station name.  The field must be located on the same
+    grid points and times as the WRF model data in W.
     
-        matched =  match_time_series(stations, st_field_name, field, mlon, mlat, mtimes)
+        matched =  match_time_series(stations, st_field_name, field, W)
         
     """
+    mlon = W.get_lons()
+    mlat = W.get_lats()
+    mtimes = W.get_times()
+    
     matched = {}
-    for s in stations.values():
-        st_fm = s[st_field_name]
-        st_times = sorted(st_fm.keys())
+    for st_name in stations:
+        s = stations[st_name]
+        st_data = s[st_field_name]
+        st_times = sorted(st_data.keys())
         i, j = s['nearest_grid_point']
         m_ts = field[:, i, j]
         match_times, indx1, _ = match_sample_times(mtimes, st_times)
         m_ts = m_ts[indx1]
-        st_ts = [ st_fm[d] for d in match_times ]
-        matched[s['name']] = (match_times, m_ts, st_ts)
+        st_ts = [ st_data[d] for d in match_times ]
+        matched[st_name] = (match_times, m_ts, st_ts, great_circle_distance(mlon[i,j], mlat[i,j], s['lon'], s['lat']))
         
     return matched
-        
 
-def great_circle_distance(lon1, lat1, lon2, lat2):
-    """
-    Computes the great circle distance between two points given as (lon1,lat1), (lon2,lat2)
-    in kilometers.
+
+def plot_stations_vs_model_ts(stations, field_name, field, W):
     
-        d = great_circle_distance(lon1, lat1, lon2, lat2)
-    """
-    rlat1, rlat2 = np.pi * lat1 / 180.0, np.pi * lat2 / 180.0
-    rlon1, rlon2 = np.pi * lon1 / 180.0, np.pi * lon2 / 180.0
+    ms_ts = match_time_series(stations, st_field_name, field, W)
+
+    # extract station time series and corresponding grid point time series
+    f = plt.figure()
+    f.subplots_adjust(hspace = 1.2)
+    i = 1
+    for station_name in ms_ts.keys():
+        match_times, m_ts, st_ts, _ = ms_ts[station_name]
+        ax = plt.subplot(4, 2, i)
+        ax.xaxis.set_major_formatter(DateFormatter('%H:%m')) 
+        plt.plot(match_times, st_ts, 'ro-', match_times, m_ts, 'gx-', linewidth = 2)
+        plt.title('%s vs. model %s' % (station_name, st_field_name))
+        for l in ax.get_xticklabels():
+            l.set_rotation(90)
+        i += 1
     
-    a = math.sin(0.5*(rlat1 - rlat2))**2 + math.cos(rlat1)*math.cos(rlat2)*math.sin(0.5*(rlon1 - rlon2))**2
-    c = 2 * math.atan2(a**0.5, (1-a)**0.5)
-    return 6371.0 * c
 
 
 if __name__ == '__main__':
 
     # load the smallest domain
-    v = load_wrf_data('../real_data/witch_creek/realfire03_d04_20071021.nc')
+    W = WRFModelData('../real_data/witch_creek/realfire03_d04_20071021.nc', None, 'US/Pacific')
 
     # read in vars
-    lat = v['XLAT'][0,:,:]
-    lon = v['XLONG'][0,:,:]
-    rain = v['RAINNC']
-    Q2 = v['Q2']
-    T2 = v['T2']
-    P = v['PSFC']
-    tm = v['Times']
+    lat = W.get_lats()
+    lon = W.get_lons()
+    rain = W['RAINNC']
+    Q2 = W['Q2']
+    T2 = W['T2']
+    P = W['PSFC']
+    tm = W.get_times()
     
     # load stations and match then to grid points
-    stations = load_stations_from_files(station_data_dir, station_list)
+    stations = load_stations_from_files(station_data_dir, station_list, 'US/Pacific')
     match_stations_to_gridpoints(stations, lon, lat)
 
     # construct basemap for rendering
-    lat_rng = (np.min(lat), np.max(lat))
-    lon_rng = (np.min(lon), np.max(lon))
-    m = Basemap(llcrnrlon=lon_rng[0],llcrnrlat=lat_rng[0],
-                urcrnrlon=lon_rng[1],urcrnrlat=lat_rng[1],
+    domain_rng = W.get_domain_extent()
+    m = Basemap(llcrnrlon=domain_rng[0],llcrnrlat=domain_rng[1],
+                urcrnrlon=domain_rng[2],urcrnrlat=domain_rng[3],
                 projection = 'mill')
 
     # compute the equilibrium moisture on grid points (for all times t)
     Ed, Ew = equilibrium_moisture(P, Q2, T2)
 
-    # show this and render stations on top
+    # show the equilibrium field and render position of stations on top
     render_spatial_field(m, lon, lat, 0.5 * (Ed[0,:,:] + Ew[0,:,:]), 'Equilibrium moisture')
-    for s in station_list:
+    for s in stations.keys():
         st = stations[s]
         slon, slat = st['lon'], st['lat']
         x, y = m(slon, slat)
         plt.plot(x, y, 'o', markersize = 8, markerfacecolor = 'magenta')
+        plt.text(x, y, s)
 
     x, y = m(lon.ravel(), lat.ravel())
     plt.plot(x, y, 'k+', markersize = 4)
 
     # part B, compare values at the same times
-    for st_field_name, field in [ ('T', T2 - 273.15), ('rain', rain), ('fuel_moisture', 50.0 * (Ed + Ew))]:
-        ms_ts = match_time_series(stations, st_field_name, field, lon, lat, tm)
+    for st_field_name, field in [ ('T', T2 - 273.15), ('fuel_moisture', 50.0 * (Ed + Ew))]:
+        plot_stations_vs_model_ts(stations, st_field_name, field, W)
+        
+    # examine fuel moisture residuals
+    fm_ts  = match_time_series(stations, 'fuel_moisture', field, W)
+    
+    # recode all of this into a matrix to fit the model
+    model_data = []
+    stat_data = []
+    weights = []
+    for s in fm_ts.keys():
+        _, mts, stts, d = fm_ts[s]
+        model_data.extend(mts)
+        stat_data.extend(stts)
+        weights.extend([1.0 / d for i in range(len(mts))])
+        
+    mdata = np.array(model_data)
+    sdata = np.array(stat_data)
+    weights = np.array(weights)
+    
+    # compute the best fit of the data
+    beta = np.sum(weights * mdata * sdata) / np.sum(weights * mdata ** 2)
+    print("Weighted lin. regression parameter is %g." % beta)
+    
+    # plot fitted model
+    plot_stations_vs_model_ts(stations, 'fuel_moisture', beta * 50 * (Ed + Ew), W)
+    
+    # now get all the residuals of the fit in each station
+    sres = {}
+    for s in fm_ts:
+        print s
+        st = stations[s]
+        _, mts, stts, _ = fm_ts[s]
+        sres[s] = (np.array(stts) - beta * np.array(mts), st['lon'], st['lat'])
 
-        # extract station time series and corresponding grid point time series
-        f = plt.figure()
-        f.subplots_adjust(hspace = 1.2)
-        i = 1
-        for station_name in ms_ts.keys():
-            match_times, m_ts, st_ts = ms_ts[station_name]
-            ax = plt.subplot(4, 2, i)
-            ax.xaxis.set_major_formatter(DateFormatter('%H:%m')) 
-            plt.plot(match_times, st_ts, 'ro-', match_times, m_ts, 'gx-', linewidth = 2)
-            plt.title('%s vs. model %s' % (station_name, st_field_name))
-            for l in ax.get_xticklabels():
-                l.set_rotation(90)
-            i += 1
-        
-        
-    for s in stations.values():
-        i, j = s['nearest_grid_point']
-        mlon = lon[i, j]
-        mlat = lat[i, j]
-        print("Distance of %s from nearest grid point: %g km" 
-                % (s['name'], great_circle_distance(mlon, mlat, st['lon'], st['lat'])))
-        
-        
+    # **********************************************************************************
+    # compute COVARIANCE between station residuals and plot this vs. distance
+    Ns = len(fm_ts.keys())
+    C = np.zeros((Ns,Ns))
+    D = np.zeros((Ns,Ns))
+    for s1, i in zip(fm_ts.keys(), range(Ns)):
+        r1, lon1, lat1 = sres[s1]
+        for s2, j in zip(fm_ts.keys(), range(Ns)):
+            r2, lon2, lat2 = sres[s2]
+            cc = np.cov(r1, r2)
+            C[i,j] = cc[0,1]
+            D[i,j] = great_circle_distance(lon1, lat2, lon2, lat2)
+
+    f = plt.figure(figsize = (8,8))
+    f.subplots_adjust(hspace = 0.5, wspace = 0.5)
+    ax = plt.subplot(221)
+    plt.imshow(C, interpolation = 'nearest')
+    plt.title('Covariance [-]')
+    plt.colorbar()
+    ax.set_xticklabels(fm_ts.keys(), rotation = 90)
+    ax.set_yticklabels(fm_ts.keys())
+    ax = plt.subplot(222)
+    plt.imshow(D, interpolation = 'nearest')
+    plt.title('Distance [km]')
+    ax.set_xticklabels(fm_ts.keys(), rotation = 90)
+    ax.set_yticklabels(fm_ts.keys())
+    plt.colorbar()
+    plt.subplot(223)
+    colors = 'rgbcmyk'
+    for r in range(D.shape[0]):
+        ndx = np.setdiff1d(np.arange(Ns), [r])
+        plt.plot(D[r,ndx], C[r,ndx], '%so' % colors[r])
+    plt.grid()
+    plt.xlabel('Distance [km]')
+    plt.ylabel('Covariance [-]')
+    plt.title('Aggregate plot of covar vs. dist')
+
+    plt.subplot(224)
+    iu1 = np.triu_indices(Ns, 1)
+    Dt = D[iu1]
+    Ct = C[iu1]
+    plt.plot(Dt, Ct, 'ro')
+    plt.grid()
+    plt.xlabel('Distance [km]')
+    plt.ylabel('Covariance [-]')
+    plt.title('Aggregate plot of covar vs. distance')
+
+    f = open('distance_vs_covariance.txt', 'w')
+    for i in range(Dt.shape[0]):
+        f.write('%g,%g\n' % (Dt[i], Ct[i]))
+    f.close() 
+
+
+    # **********************************************************************************
+    # compute CORRELATION COEFFICIENT between station residuals and plot this vs. distance
+    Ns = len(fm_ts.keys())
+    C = np.zeros((Ns,Ns))
+    D = np.zeros((Ns,Ns))
+    for s1, i in zip(fm_ts.keys(), range(Ns)):
+        r1, lon1, lat1 = sres[s1]
+        for s2, j in zip(fm_ts.keys(), range(Ns)):
+            r2, lon2, lat2 = sres[s2]
+            cc = np.corrcoef(r1, r2)
+            C[i,j] = cc[0,1]
+            D[i,j] = great_circle_distance(lon1, lat2, lon2, lat2)
+
+    f = plt.figure(figsize = (8,8))
+    f.subplots_adjust(hspace = 0.5, wspace = 0.5)
+    ax = plt.subplot(221)
+    plt.imshow(C, interpolation = 'nearest')
+    plt.clim([0.0, 1.0])
+    plt.title('Correlation coefficient [-]')
+    plt.colorbar()
+    ax.set_xticklabels(fm_ts.keys(), rotation = 90)
+    ax.set_yticklabels(fm_ts.keys())
+    ax = plt.subplot(222)
+    plt.imshow(D, interpolation = 'nearest')
+    plt.title('Distance [km]')
+    ax.set_xticklabels(fm_ts.keys(), rotation = 90)
+    ax.set_yticklabels(fm_ts.keys())
+    plt.colorbar()
+    plt.subplot(223)
+    colors = 'rgbcmyk'
+    for r in range(D.shape[0]):
+        ndx = np.setdiff1d(np.arange(Ns), [r])
+        plt.plot(D[r,ndx], C[r,ndx], '%so' % colors[r])
+    plt.ylim([0.0, 1.0])
+    plt.grid()
+    plt.xlabel('Distance [km]')
+    plt.ylabel('Correlation coefficient [-]')
+    plt.title('Aggregate plot of cc vs. dist')
+
+    plt.subplot(224)
+    iu1 = np.triu_indices(Ns, 1)
+    Dt = D[iu1]
+    Ct = C[iu1]
+    plt.plot(Dt, Ct, 'ro')
+    plt.plot(Dt, 0.8166 - 0.0052 * Dt, 'r-')
+    plt.plot(Dt, 0.8565 - 0.0063 * Dt, 'g-')
+    plt.plot(Dt, 0.8729 - 0.0068 * Dt, 'b-')
+    plt.ylim([0.0, 1.0])
+    plt.legend(['Data', 'OLS', 'Huber', 'Bisquare'], loc = 'lower left', prop = { 'size' : 10 } )
+    plt.grid()
+    plt.xlabel('Distance [km]')
+    plt.ylabel('Correlation coefficient [-]')
+    plt.title('Aggregate plot of cc vs. distance')
+    
+    f = open('distance_vs_covariance.txt', 'w')
+    for i in range(Dt.shape[0]):
+        f.write('%g,%g\n' % (Dt[i], Ct[i]))
+    f.close() 
+    
     plt.show()
     
--- spatial_field_model/spatial_model_utilities.py
+++ spatial_field_model/spatial_model_utilities.py
@@ -7,12 +7,28 @@ Created on Sun Oct 28 18:14:57 2012
 
 import numpy as np
 import os.path
-import netCDF4
 import matplotlib.pyplot as plt
 import codecs
 import re
 from datetime import datetime, timedelta
 import unicodedata
+import pytz
+import math
+
+
+def great_circle_distance(lon1, lat1, lon2, lat2):
+    """
+    Computes the great circle distance between two points given as (lon1,lat1), (lon2,lat2)
+    in kilometers.
+    
+        d = great_circle_distance(lon1, lat1, lon2, lat2)
+    """
+    rlat1, rlat2 = np.pi * lat1 / 180.0, np.pi * lat2 / 180.0
+    rlon1, rlon2 = np.pi * lon1 / 180.0, np.pi * lon2 / 180.0
+    
+    a = math.sin(0.5*(rlat1 - rlat2))**2 + math.cos(rlat1)*math.cos(rlat2)*math.sin(0.5*(rlon1 - rlon2))**2
+    c = 2 * math.atan2(a**0.5, (1-a)**0.5)
+    return 6371.0 * c
 
 
 def equilibrium_moisture(P, Q, T):
@@ -36,6 +52,7 @@ def equilibrium_moisture(P, Q, T):
     return Ed * 0.01, Ew * 0.01
     
 
+
 def render_spatial_field(m, lon, lat, field, title):
     """
     Render a geo field over a basemap.  However the standard basemap
@@ -57,31 +74,8 @@ def render_spatial_field(m, lon, lat, field, title):
     plt.title(title)
 
 
-def load_wrf_data(data_file,
-                  var_names = ['T2', 'Q2', 'PSFC', 'XLAT', 'XLONG', 'RAINNC', 'Times']):
-    """
-    Load required variables from the file data_file.  A list of variables
-    is either supplied or the default list is used which contains the following
-    variables: 'T2', 'Q2', 'PSFC', 'XLAT', 'XLONG', 'RAINNC', 'Times'
-    """
-    v = {}
-    d = netCDF4.Dataset(os.path.join(data_file))
-    for vname in var_names:
-        v[vname] = d.variables[vname][:,...]
-    d.close()
-    
-    # recode times into datetime objects
-    if v.has_key('Times'):
-        t = v['Times']
-        tp = []
-        for tm in t:
-            tp.append(datetime.strptime(''.join(tm), '%Y-%m-%d_%H:%M:%S'))
-        v['Times'] = tp
 
-    return v
-
-
-def load_station_data(station_file):
+def load_station_data(station_file, tz):
     """
     Load all available fuel moisture data from the station information.
     """
@@ -118,7 +112,7 @@ def load_station_data(station_file):
 
         # read in and parse date
         l = f.readline()
-        date = datetime.strptime(l.strip(), '%B %d, %Y')
+        date = datetime.strptime(l.strip(), '%B %d, %Y').replace(tzinfo = tz)
         
         # read lines until a line starts with daily
         while l[0] < '0' or l[0] > '9' and len(l) > 0:
@@ -158,12 +152,14 @@ def load_station_data(station_file):
     return station_data
     
 
+
 def find_closest_grid_point(slon, slat, glon, glat):
     """
-    Finds the closest grid piont to the given station longitude/lattitude.
+    Finds the closest grid point to the given station longitude/lattitude.
     """
     closest = np.argmin((slon - glon)**2 + (slat - glat)**2)
     return np.unravel_index(closest, glon.shape)
+
 
 
 def match_stations_to_gridpoints(sts, lon, lat):
@@ -173,23 +169,34 @@ def match_stations_to_gridpoints(sts, lon, lat):
     """
     for s in sts.values():
         i, j = find_closest_grid_point(s['lon'], s['lat'], lon, lat)
+        d = great_circle_distance(s['lon'], s['lat'], lon[i,j], lat[i,j])
         s['nearest_grid_point'] = (i,j)
+        s['dist_nearest_grid_point'] = d
         
 
-def load_stations_from_files(station_data_dir, station_list):
+
+def load_stations_from_files(station_data_dir, station_list, tz_name):
     """
     Loads station data from files given to me by AK and stores all station data
-    in a dictionary indexed by station file names.
+    in a dictionary indexed by station file names.  All the times in the files
+    are considered to be relative to the time zone tz_name.
+    FIXME: assuming all stations are in the same time zone is dangerous! 
     
-        stations = load_stations_from_files(station_data_dir, station_list)
+        stations = load_stations_from_files(station_data_dir, station_list, tz_name)
     
     """
     stations = {}
+    tz = pytz.timezone(tz_name) 
     for s in station_list:
-        st = load_station_data(os.path.join(station_data_dir, s))
+        st = load_station_data(os.path.join(station_data_dir, s), tz)
+        if s.endswith('_Moisture'):
+            s = s[:-9]
+        if s.endswith('_Fuel'):
+            s = s[:-5]
         stations[s] = st
 
     return stations
+
 
 
 def match_sample_times(tm1, tm2):
@@ -206,11 +213,11 @@ def match_sample_times(tm1, tm2):
     indx1 = []
     indx2 = []
     while i < len(tm1) and j < len(tm2):
-        while i < len(tm1) and tm1[i] < tm2[j]:
+        if tm1[i] < tm2[j]:
             i += 1
-        while j < len(tm2) and i < len(tm1) and tm1[i] > tm2[j]:
+        elif tm1[i] > tm2[j]:        
             j += 1
-        if i < len(tm1) and j < len(tm2) and tm1[i] == tm2[j]:
+        else:
             isect.append(tm1[i])
             indx1.append(i)
             indx2.append(j)
